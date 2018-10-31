f0af3e41ff87c4a6a53880d42868b3fb101b16c2
pyy <869849673@qq.com>
2018-10-31 16:16:04 +0800

    zuul 路由网关  并自定义过滤

---
+++ eureka-zuul-client/src/main/java/com/example/eurekazuulclient/filter/MyFilter.java
@@ -0,0 +1,58 @@
+package com.example.eurekazuulclient.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/**
+ * @Auther: 庞洋洋
+ * @Date: 2018/10/31 15:34
+ * @Description:
+ */
+@Component
+public class MyFilter extends ZuulFilter{
+
+    private static final Logger logger = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        Object accessToken = request.getParameter("token");
+        if (accessToken == null){
+            logger.warn("token  is  null");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+        logger.info("OK!!!");
+        return null;
+    }
+}
