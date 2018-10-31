651cba8df52fb2da2299b6302f4175e670aed8b6
pyy <869849673@qq.com>
2018-10-31 11:34:14 +0800

    在restTemplate 和 Ribbon 上使用 hystrix 熔断器

--- eureka-ribbon-client/src/main/java/com/example/eurekaribbonclient/EurekaRibbonClientApplication.java
+++ eureka-ribbon-client/src/main/java/com/example/eurekaribbonclient/EurekaRibbonClientApplication.java
@@ -3,9 +3,11 @@ package com.example.eurekaribbonclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableHystrix
 public class EurekaRibbonClientApplication {
 
 	public static void main(String[] args) {
--- eureka-ribbon-client/src/main/java/com/example/eurekaribbonclient/service/RibbonService.java
+++ eureka-ribbon-client/src/main/java/com/example/eurekaribbonclient/service/RibbonService.java
@@ -1,5 +1,6 @@
 package com.example.eurekaribbonclient.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,7 +16,12 @@ public class RibbonService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hi(String name){
         return restTemplate.getForObject("http://eureka-client/hi?name="+name, String.class);
+    }
+
+    public String hiError(String name){
+        return "hi " + name + " ,sorry ,error!";
     }
 }
