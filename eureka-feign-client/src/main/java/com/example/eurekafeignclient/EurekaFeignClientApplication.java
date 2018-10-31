05a5e524bc319a751ecfaa06313f5d0f1e1ea463
pyy <869849673@qq.com>
2018-10-31 13:29:07 +0800

    在 feign 上使用 hystrix dashboard 监控熔断器状态

--- eureka-feign-client/src/main/java/com/example/eurekafeignclient/EurekaFeignClientApplication.java
+++ eureka-feign-client/src/main/java/com/example/eurekafeignclient/EurekaFeignClientApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrixDashboard
 public class EurekaFeignClientApplication {
 
 	public static void main(String[] args) {
